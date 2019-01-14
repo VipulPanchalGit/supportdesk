89b663ad35a093468c0bb76ce59c5ca3fcb9bf0d
mac user <macuser@VipulMacPro.local>
2019-01-14 00:50:29 -0800

    Adding test cases

---
+++ src/main/java/com/vipul/supportdesk/faq/com/vipul/supportdesk/faq/util/CommonUtils.java
@@ -0,0 +1,4 @@
+package com.vipul.supportdesk.faq.com.vipul.supportdesk.faq.util;
+
+public class CommonUtils {
+}
---
+++ src/main/java/com/vipul/supportdesk/faq/persistence/FaqRepositorySetUp.java
@@ -0,0 +1,4 @@
+package com.vipul.supportdesk.faq.persistence;
+
+public class FaqRepositorySetUp {
+}
