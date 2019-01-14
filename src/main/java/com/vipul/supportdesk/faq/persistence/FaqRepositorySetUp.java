a837b4ab6bcbc0a6a121d6a8a6223cee07e1f760
mac user <macuser@VipulMacPro.local>
2019-01-14 00:52:28 -0800

    Adding test cases

--- src/main/java/com/vipul/supportdesk/faq/persistence/FaqRepositorySetUp.java
+++ src/main/java/com/vipul/supportdesk/faq/persistence/FaqRepositorySetUp.java
@@ -1,4 +1,79 @@
 package com.vipul.supportdesk.faq.persistence;
 
-public class FaqRepositorySetUp {
+import com.vipul.supportdesk.faq.com.vipul.supportdesk.faq.util.CommonUtils;
+import com.vipul.supportdesk.faq.domain.CategoryFAQ;
+import com.vipul.supportdesk.faq.domain.FaqData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Component
+public class FaqRepositorySetUp
+{
+    @Autowired
+    private FaqRepository faqRepository;
+
+    @Autowired
+    private CommonUtils commonUtils;
+
+    public void tearDownFAQ()
+    {
+        System.out.println();
+        System.out.println("Deleting all FAQ data");
+        faqRepository.deleteAll();
+    }
+
+    public void setUpFAQ()
+    {
+        tearDownFAQ();
+
+        System.out.println();
+        System.out.println("Adding FAQ data");
+        faqRepository.save(getFaqData("How to activate my account?"
+                , "Call 1-800-ATV-ACCT to activate your account"
+                , CategoryFAQ.ACCOUNT
+                , null)
+        );
+        faqRepository.save(getFaqData("How to cancel my account?"
+                , "Call 1-800-ATV-ACCT to cancel your account"
+                , CategoryFAQ.ACCOUNT
+                , null)
+        );
+        faqRepository.save(getFaqData("How to report fraud activity on my account?"
+                , "Call 1-800-ATV-ACCT to report fraud activity on your account"
+                , CategoryFAQ.FRAUD
+                , null)
+        );
+        faqRepository.save(getFaqData("How to subscribe to newsfeed?"
+                , "Visit www.atv.com/newsfeed/follow/ to subscribe to newsfeed"
+                , CategoryFAQ.SUBSCRIPTION
+                , null)
+        );
+        faqRepository.save(getFaqData("How to reset my account password?"
+                , "Call 1-800-ATV-ACCT to reset your account password or visit www.atv.com/password-reset/ "
+                , CategoryFAQ.PASSWORD
+                , null)
+        );
+    }
+
+    private FaqData getFaqData(String question, String answer, CategoryFAQ categoryFAQ, List<String> labels)
+    {
+        FaqData entity = new FaqData(commonUtils.getNextId());
+        entity.setCategory(categoryFAQ);
+        if(labels != null && !labels.isEmpty())
+        {
+            entity.setLabels(labels);
+        } else {
+            entity.setLabels(commonUtils.getDefaultLabel(categoryFAQ));
+        }
+
+        entity.setQuestion(question);
+        entity.setAnswer(answer);
+        entity.setLinkedArticle(null);
+
+        return entity;
+    }
 }
--- src/main/java/com/vipul/supportdesk/faq/persistence/SupportArticleRepositorySetUp.java
+++ src/main/java/com/vipul/supportdesk/faq/persistence/SupportArticleRepositorySetUp.java
@@ -1,4 +1,89 @@
 package com.vipul.supportdesk.faq.persistence;
 
-public class SupportArticleRepositorySetUp {
+import com.vipul.supportdesk.faq.com.vipul.supportdesk.faq.util.CommonUtils;
+import com.vipul.supportdesk.faq.domain.CategoryFAQ;
+import com.vipul.supportdesk.faq.domain.SupportArticle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SupportArticleRepositorySetUp
+{
+    @Autowired
+    private SupportArticleRepository supportArticleRepository;
+
+    @Autowired
+    private CommonUtils commonUtils;
+
+    public void tearDownSupportArticles()
+    {
+        System.out.println();
+        System.out.println("Deleting all support articles");
+        supportArticleRepository.deleteAll();
+    }
+
+    public void setUpSupportArticles()
+    {
+        tearDownSupportArticles();
+
+        System.out.println();
+        System.out.println("Adding support articles");
+        supportArticleRepository.save(getSupportArticle("Password reset steps"
+                , "Follow these steps to reset password on www.atv.com/password-reset/ \n "
+                        + "Step 1: Enter your account number \n"
+                        + "Step 2: Choose verification code via SMS or EMAIL \n"
+                        + "Step 3: Enter verification code \n"
+                        + "Step 4: Provide new password \n"
+                , CategoryFAQ.PASSWORD
+                , null
+                )
+        );
+        supportArticleRepository.save(getSupportArticle("How to subscribe newsfeed steps"
+                , "Follow these steps to subscribe on www.atv.com/newsfeed/ \n "
+                        + "Step 1: Enter your account number \n"
+                        + "Step 2: Choose all desired newsfeed\n"
+                        + "Step 3: Click on save subscriptions \n"
+                , CategoryFAQ.SUBSCRIPTION
+                , null
+                )
+        );
+        supportArticleRepository.save(getSupportArticle("Shipment tracking steps"
+                , "Follow these steps to track your pacakge on www.atv.com/shipment/ \n "
+                        + "Step 1: Enter your account number \n"
+                        + "Step 2: Enter shipment tracking number\n"
+                        + "Step 3: Click on track-my-shipment\n"
+                , CategoryFAQ.SHIPMENT
+                , null
+                )
+        );
+    }
+
+
+    private SupportArticle getSupportArticle(String articleTitle, String articleText, CategoryFAQ categoryFAQ, List<String> labels)
+    {
+        SupportArticle entity = new SupportArticle(commonUtils.getNextId());
+        entity.setCategory(categoryFAQ);
+        if(labels != null && !labels.isEmpty())
+        {
+            entity.setLabels(labels);
+        } else {
+            entity.setLabels(commonUtils.getDefaultLabel(categoryFAQ));
+        }
+        if(articleText == null || articleText.isEmpty())
+        {
+            entity.setArticleText("NO ARTICLE TEXT PROVIDED !!!");
+        } else {
+            entity.setArticleText(articleText);
+        }
+
+        if(articleTitle == null || articleTitle.isEmpty())
+        {
+            entity.setArticleTitle("MISSING ARTICLE TITLE!!!");
+        } else {
+            entity.setArticleTitle(articleTitle);
+        }
+        return entity;
+    }
 }
